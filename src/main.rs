4a3e0a22d82086cbd76f40e2ec353a6f62b89459
Christopher Fleetwood <fleetwoodpersonal@gmail.com>
2021-06-26 18:37:05 +0100

    working

--- src/main.rs
+++ src/main.rs
@@ -8,82 +8,144 @@ pub const REGISTER_NAMES: [&str; 32] = [
     "t5", "t6",
 ];
 
-#[cfg_eval(rustfmt, rustfmt_skip)]
 pub enum OpCode {
-    LUI = 0x37,
-    AUIPC = 0x17,
-    JAL = 0x6F,
-    JALR = 0x67,
-    BRANCH = 0x63,
-    LOAD = 0x03,
-    STORE = 0x23,
-    IMM = 0x13,
-    OP = 0x33,
-    FENCE = 0x0F,
-    EX = 0x73,
+    //RV32I
+    LUI,
+    AUIPC,
+    JAL,
+    JALR,
+    BEQ,
+    BNE,
+    BLT,
+    BGE,
+    BLTU,
+    BGEU,
+    LB,
+    LH,
+    LW,
+    LBU,
+    LHU,
+    SB,
+    SH,
+    SW,
+    ADDI,
+    SLTI,
+    SLTIU,
+    XORI,
+    ORI,
+    ADNI,
+    SLLI,
+    SRLI,
+    SRAI,
+    ADD,
+    SUB,
+    SLL,
+    SLT,
+    SLTU,
+    XOR,
+    SRL,
+    SRA,
+    OR,
+    AND,
+    FENCE,
+    ECALL,
+    EBREAK,
+
+    //RV64I
+    LWU,
+    LD,
+    SD,
+    ADDIW,
+    SLLIW,
+    SRLIW,
+    SRAIW,
+    ADDW,
+    SUBW,
+    SLLW,
+    SRLW,
+    SRAW
 }
 
-#[cfg_eval(rustfmt, rustfmt_skip)]
 pub enum Funct3 {
-    ADD = 0x0,
-    SUB = 0x0,
-    ADDI = 0x0,
-    SLLI = 0x1,
-    SLT = 0x2,
-    SLTI = 0x2,
-    SLTU = 0x3,
-    SLTUI = 0x3,
-    
-    XOR = 0x4,
-    XORI = 0x4,
-    
-    SRL = 0x5,
-    SRLI = 0x5,
-    SRA = 0x5,
-    SRAI = 0x5,
-    
-    OR = 0x6,
-    ORI = 0x6,
-    AND = 0x7,
-    ANDI = 0x7,
-    
-    BEQ = 0x0,
-    BNE = 0x1,
-    BLT = 0x4,
-    BGE = 0x5,
-    BLTU = 0x6,
-    BGEU = 0x7,
+    JAL,
+    JALR,
+    BEQ,
+    BNE,
+    BLT,
+    BGE,
+    BLTU,
+    BGEU,
+    LB,
+    LH,
+    LW,
+    LBU,
+    LHU,
+    SB,
+    SH,
+    SW,
+    ADDI,
+    SLTI,
+    SLTIU,
+    XORI,
+    ORI,
+    ADNI,
+    SLLI,
+    SRLI,
+    SRAI,
+    ADD,
+    SUB,
+    SLL,
+    SLT,
+    SLTU,
+    XOR,
+    SRL,
+    SRA,
+    OR,
+    AND,
+    FENCE,
+    ECALL,
+    EBREAK,
 
-    LB = 0x0,
-    SB = 0x0,
-    LH = 0x1,
-    SH = 0x1,
-    LW = 0x2,
-    SW = 0x2,
-    LBU = 0x4,
-    LHU = 0x5,
-
+    //RV64I
+    LWU,
+    LD,
+    SD,
+    ADDIW,
+    SLLIW,
+    SRLIW,
+    SRAIW,
+    ADDW,
+    SUBW,
+    SLLW,
+    SRLW,
+    SRAW
 }
 
-#[cfg_eval(rustfmt, rustfmt_skip)]
 pub enum Funct7 {
-    SLLI = 0x00,
-    SRLI = 0x00,
-    SRAI = 0x10,
-    SLLIW = 0x00,
-    SRLIW = 0x00,
-    SRAIW = 0x10,
-    ADDW = 0x00,
-    SUBW = 0x20,
-    SLLW = 0x00,
-    SRLW = 0x00,
-    SRAW = 0x20,
+    SLLI,
+    SRLI,
+    SRAI,
+    SLLIW,
+    SRLIW,
+    SRAIW,
+    ADDW,
+    SUBW,
+    SLLW,
+    SRLW,
+    SRAW,
 }
+
+struct Instruction(Opcode,)
 
 
 struct CPU {
     regfile: [u64; 32],
     pc: u64,
+    dram: Vec<u8>
+}
+
+pub fn get_bits(inst: u32, start: u32, end: u32) -> u32 {
+    (inst >> end) & ((1 << (start - end + 1)) - 1)
 }
 
 fn step(cpu: CPU) {
@@ -103,11 +165,33 @@ impl CPU {
         CPU { regfile, pc: 0 }
     }
 
-    fn get_bits(inst: u32) {}
+    fn fetch(&self) -> u32 {
+        
+    }
 
-    fn fetch(&self) -> Result<u64> {}
+    //page 130 for instruction format
+    fn execute(&self, inst: u32) {
+        let opcode = get_bits(inst, 0, 6);
+        let rd = get_bits(inst, 7, 12) as usize;
+        let rs1 = get_bits(inst, 15, 20) as usize;
+        let rs2 = get_bits(inst, 20,25) as usize;
 
-    fn execute(&self, inst: u64) {}
+        self.regs[0] = 0;
+
+        println!("OPCODE: {:?}", opcode);
+        println!("RD: {:?}", rd);
+        println!("RS1: {:?}", rs1);
+        println!("RS2: {:?}", rs2);
+
+        match opcode {
+            OpCode::ADDI => {
+                let imm = get_bits(inst, 20, 31) >> 20;
+                self.regs[rs1].wrapping_add(imm); 
+            }
+            OpCode::ADD => {self.regs[rd] = self.regs[rs1].wrapping_add(self.regs[rs2]);}
+        }
+
+    }
 
     pub fn dump_registers(&self) {
         for (regidx, reg) in self.regfile.iter().enumerate() {
@@ -120,18 +204,6 @@ impl CPU {
 }
 
 fn main() -> io::Result<()> {
-    ///Here need to read in all of the instruction tests
-    /// perhaps with an elf crate
-    /// Need to read in all the tests from the risc-v folder
-    /// Need a CPU struct to hold all the registers
-    /// How many registers does the CPU have?
-    /// How does memory work on the CPU
-    /// IF, ID, EX, MEM, WB
-    ///
-    /// Need to read the files in, might be an ELF crate
-    /// dump_registers (pp)
-    /// fetch function
-    /// execute function
     let cpu = CPU::new();
     cpu.dump_registers();
 
